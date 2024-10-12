ac7ceec2323a365d0cfefcc67f34018d841cfa2c
Nassim <achab.nassim.solicode@gmail.com>
2024-10-12 14:57:33 +0100

    fixed npm modules, added some more projects

--- src/containerComponent/projectsSection/ProjectSection.jsx
+++ src/containerComponent/projectsSection/ProjectSection.jsx
@@ -7,29 +7,36 @@ const ProjectSection = () => {
     <div id='projectSectionContainer'>
         <p id='projectHeader'>Projects</p>
         <ProjectComponent
+        name={"Breakout"} 
+        projectSource={"https://github.com/AchabNassim/Breakout_game"}
+        technologies={["Php", "Javascript", "Html", "Css", "Mysql"]} 
+        description={"Here I'll describe in a paragraph or so what my project is, what it does, and most importantly WHO IT SERVES! Describe the problem it fixes and for who it fixes it for. If you worked on a team. Fixes and for who it fixes it for. Here I'll describe in a paragraph or so what my project is"}
+        videoSource={"project.mp4"}
+        />
+
+        <ProjectComponent
           name={"Ecommerce backend api"} 
           projectSource={"https://github.com/AchabNassim/NodeAuth"}
-          technologies={["Nodejs", "ExpressJs", "Express-session", "Mongodb", "Mongoose"]} 
+          technologies={["Nodejs", "ExpressJs", "Express-session", "Postgresql"]} 
           description={"Here I'll describe in a paragraph or so what my project is, what it does, and most importantly WHO IT SERVES! Describe the problem it fixes and for who it fixes it for. If you worked on a team. Fixes and for who it fixes it for. Here I'll describe in a paragraph or so what my project is."}
           videoSource={"project.mp4"}
          />
 
-         <ProjectComponent
-          name={"Productive Notes"} 
-          projectSource={"https://github.com/AchabNassim/productivityApp"}
-          technologies={["React", "Nodejs", "ExpressJs", "Mongodb", "Zustand", "Material-ui"]} 
-          description={"Here I'll describe in a paragraph or so what my project is, what it does, and most importantly WHO IT SERVES! Describe the problem it fixes and for who it fixes it for. If you worked on a team. Fixes and for who it fixes it for. Here I'll describe in a paragraph or so what my project is."}
-          videoSource={"project.mp4"}
-         />
+        <ProjectComponent
+        name={"Productive Notes"} 
+        projectSource={"https://github.com/AchabNassim/productivityApp"}
+        technologies={["React", "Material-ui", "Zustand"]} 
+        description={"Here I'll describe in a paragraph or so what my project is, what it does, and most importantly WHO IT SERVES! Describe the problem it fixes and for who it fixes it for. If you worked on a team. Fixes and for who it fixes it for. Here I'll describe in a paragraph or so what my project is."}
+        videoSource={"project.mp4"}
+        />
 
-         <ProjectComponent
-          name={"Breakout"} 
-          projectSource={"https://github.com/AchabNassim/Breakout_game"}
-          technologies={["Php", "Javascript", "Html", "Css", "Mysql"]} 
-          description={"Here I'll describe in a paragraph or so what my project is, what it does, and most importantly WHO IT SERVES! Describe the problem it fixes and for who it fixes it for. If you worked on a team. Fixes and for who it fixes it for. Here I'll describe in a paragraph or so what my project is"}
-          videoSource={"project.mp4"}
-         />
-
+        <ProjectComponent
+        name={"MinimalTyper"} 
+        projectSource={"https://github.com/AchabNassim/productivityApp"}
+        technologies={["Html", "Css", "Javascript"]} 
+        description={"Here I'll describe in a paragraph or so what my project is, what it does, and most importantly WHO IT SERVES! Describe the problem it fixes and for who it fixes it for. If you worked on a team. Fixes and for who it fixes it for. Here I'll describe in a paragraph or so what my project is."}
+        videoSource={"project.mp4"}
+        />
         {/* <ProjectComponent
           name={"Breakout"} 
           projectSource={""}
