ae2d0ed14464bdff7a01aadc063304ca3a9af4e7
Nassim <achab.nassim.solicode@gmail.com>
2024-10-13 15:16:01 +0100

    preload added to reduce load time

--- src/containerComponent/projectsSection/projectComponent/videoComponent/VideoComponent.jsx
+++ src/containerComponent/projectsSection/projectComponent/videoComponent/VideoComponent.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React, {useRef, useState, useEffect} from "react";
 import './videoComponent.css'
 
 const VideoComponent = ({videoSource, poster}) => {
-
     return (
-        <video className="projectVideo" muted controls >
+        <video className="projectVideo" muted controls preload="metadata" >
             <source src={videoSource} type="video/mp4" />
             Your browser does not support the video tag.
         </video>
