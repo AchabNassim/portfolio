0a0f4d384ca387f344b2288f7662080aae17e297
Nassim <achab.nassim.solicode@gmail.com>
2024-10-17 21:53:24 +0100

    added about section, still lacks styling and integration into the about section

--- src/containerComponent/aboutSection/AboutSection.jsx
+++ src/containerComponent/aboutSection/AboutSection.jsx
@@ -4,21 +4,9 @@ import './aboutSection.css';
 const AboutSection = () => {
     return (
         <div id="aboutSectionContainer">
-            <div id="aboutContainer">
-                <p className="smallHeader">About</p>
-                <p id="aboutContent">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porttitor commodo orci, et aliquet turpis elementum vitae. Nunc a ante libero. Mauris a elit sit amet mauris malesuada fringilla ut non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam facilisis arcu sit amet diam commodo convallis. Etiam nunc enim, varius luctus imperdiet id, placerat ac lacus. Etiam id magna imperdiet, consectetur eros sit amet, euismod arcu. Nunc in massa a risus mollis euismod. Etiam nec mauris non justo tincidunt blandit eu in arcu. Ut eu ex pretium felis imperdiet ultricies fermentum non nulla. In blandit libero in ligula ullamcorper molestie. </p>
-            </div>
-            <div id="educationContainer">
-                <p className="smallHeader"><img src="/public/educationSvg.svg" alt="education" className="aboutSvg"/> Education</p>
-                <ul id="educationUl">
-                    <li className="educationLi"></li>
-                    <li className="educationLi"></li>
-                </ul>
-            </div>
-            <div id="certificatesContainer">
-                <p className="smallHeader"><img src="/public/certificateSvg.svg" alt="certificate" className="aboutSvg"/> Certificates</p>
-
-            </div>
+            <p className="paragHeader">About</p>
+            <p className="paragAbout">I started programming as a hobby almost four years ago, and since then, I've built a solid toolset and deep understanding of computers and programming. Currently, I'm a student at <span className="highlight"><a target="_blank" href="https://1337.ma/en/" className="aboutLink">1337</a></span> programming school. You can download my CV <span className="highlight"><a target="_blank" href="https://1337.ma/en/" className="aboutLink">here.</a></span><br />
+            Outside of programming, I enjoy sports, cooking, and taking naps.</p>
         </div>
     )
 
