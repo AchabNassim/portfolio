ee81b837a6c98afe57121a0bd041662d8827affa
Nassim <achab.nassim.solicode@gmail.com>
2024-11-23 15:12:43 +0100

    updated project description / images in webp format now

--- src/containerComponent/projectsSection/ProjectSection.jsx
+++ src/containerComponent/projectsSection/ProjectSection.jsx
@@ -11,9 +11,9 @@ const ProjectSection = () => {
         name={"Breakout"} 
         projectSource={"https://github.com/AchabNassim/Breakout_game"}
         technologies={["Php", "Javascript", "Html", "Css", "Mysql"]} 
-        description={"Breakout game clone using JavaScript and PHP. Players can easily sign up and log in to save their score and have access to the leaderboard where players can check their scores against friends and other gamers. This project allowed me to explore game development using javascript canvas, backend integration and user authentication using php and sessions"}
+        description={"Breakout game clone using JavaScript and PHP. Players can easily sign up and log in to save their score and have access to the leaderboard where players can check their scores against friends and other gamers."}
         videoSource={null}
-        poster={"showcase/screenshots/game.png"}
+        poster={"showcase/screenshots/game.webp"}
         />
 
         <ProjectComponent
@@ -22,7 +22,7 @@ const ProjectSection = () => {
         technologies={["Nodejs", "ExpressJs", "React", "Mongodb"]} 
         description={"A note-taking app allowing users to authenticate, organize, and manage notes with features like archiving, pinning, and filtering. Built with React for the frontend and Node.js/MongoDB for the backend."}
         videoSource={"showcase/videos/notes.mp4"}
-        poster={"showcase/screenshots/notes.png"}
+        poster={"showcase/screenshots/notes.webp"}
         />
 
         <ProjectComponent
@@ -31,7 +31,7 @@ const ProjectSection = () => {
           technologies={["Nodejs", "ExpressJs", "Express-session", "Postgresql"]} 
           description={"A weather-based recommendation API providing personalized outfit suggestions. Includes secure API key generation and management, with role-based access and efficient database handling."}
           videoSource={null}
-          poster={"showcase/screenshots/api.png"}
+          poster={"showcase/screenshots/api.webp"}
         />
 
         {/* <ProjectComponent
@@ -40,7 +40,7 @@ const ProjectSection = () => {
           technologies={["html, css, javascript"]} 
           description={"A typing speed website built with HTML, CSS, and JavaScript that calculates WPM and accuracy as users type. It features an interactive SVG keyboard that highlights keys pressed, with real-time animations enhancing the typing experience. This project strengthened my JavaScript skills and my understanding of DOM manipulation and CSS animations."}
           videoSource={"showcase/videos/proTyper.mp4"}
-          poster={"showcase/screenshots/keyboard.png"}
+          poster={"showcase/screenshots/keyboard.webp"}
         /> */}
 
         <ProjectComponent
@@ -49,7 +49,7 @@ const ProjectSection = () => {
           technologies={["C++"]} 
           description={"An HTTP server built with C++ 98, focusing on the HTTP protocol, socket programming, and parsing requests/responses. This project solidified my understanding of server-side development while also improving my object oriented programming skills."}
           videoSource={null}
-          poster={"showcase/screenshots/webserver.png"}
+          poster={"showcase/screenshots/webserver.webp"}
          />
 
     </div>
--- src/containerComponent/projectsSection/projectComponent/videoComponent/MediaComponent.jsx
+++ src/containerComponent/projectsSection/projectComponent/videoComponent/MediaComponent.jsx
@@ -4,7 +4,7 @@ import './mediaComponent.css'
 const MediaComponent = ({videoSource, poster}) => {
     // console.log(videoSource);
     return ( videoSource !== null ?
-            <video className="mediaContainer hiddenVideo" muted controls preload="metadata" poster={poster} onLoadedData={(e) => console.log(e.target.classList.remove("hiddenVideo"))}>
+            <video className="mediaContainer" muted controls preload="metadata" poster={poster}>
                 <source src={videoSource} type="video/mp4"  />
                 Your browser does not support the video tag.
             </video> 
